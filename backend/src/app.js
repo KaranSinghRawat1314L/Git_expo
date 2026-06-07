f78ae631f1e96414a58ee4049ea2e5f87f78f579
Karan Singh Rawat <karansinghrwt75@gmail.com>
2026-06-07 13:33:20 +0530

    converted api link in env variable and added cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -6,12 +7,16 @@ const githubRoutes = require("./routes/github.routes");
 
 const app = express();
 app.use(morgan("dev"));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 app.use(express.json());
 
 app.use("/api/github", githubRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
